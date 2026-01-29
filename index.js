2c5668becd73db44eddf90e9199ecc75761dfe9d
Serhii Frolov <larzofficialgames@gmail.com>
2026-01-29 11:42:28 +0200

    add chat function

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const { Server } = require("socket.io");
 
 const PORT = process.env.PORT || 3000;
 
-// Веб-сервер
 const httpServer = http.createServer((req, res) => {
     if (req.url === "/") {
         res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
@@ -15,32 +14,30 @@ const httpServer = http.createServer((req, res) => {
     }
 });
 
-// Socket.IO
 const io = new Server(httpServer, {
     cors: { origin: "*", methods: ["GET", "POST"] },
 });
 
 console.log(`🌉 BRIDGE | Запуск на порту ${PORT}`);
 
-// === ГОЛОВНА ЛОГІКА ===
 io.on("connection", (socket) => {
     console.log(`🔌 Клієнт підключився: ${socket.id}`);
 
-    // 1. Лист персонажа
+    // --- ЛИСТ ПЕРСОНАЖА ---
     socket.on("request_sheet_data", (id) => io.emit("request_sheet_data", id));
     socket.on("receive_sheet_data", (data) => io.emit("receive_sheet_data", data));
 
-    // 2. Кидки
+    // --- КИДКИ З ТЕЛЕФОНУ ---
     socket.on("mobile_roll_skill", (data) => io.emit("mobile_roll_skill", data));
     socket.on("mobile_roll_ability", (data) => io.emit("mobile_roll_ability", data));
-
-    // 3. Предмети
     socket.on("mobile_use_item", (data) => io.emit("mobile_use_item", data));
-
-    // 4. ШКОДА (Обов'язково має бути тут!)
     socket.on("mobile_roll_damage", (data) => io.emit("mobile_roll_damage", data));
 
-}); // <--- ФУНКЦІЯ ЗАКРИВАЄТЬСЯ ТУТ. ВАЖЛИВО!
+    // --- 🆕 ЧАТ (FOUNDRY -> PHONE) ---
+    // Сервер просто пересилає повідомлення всім
+    socket.on("foundry_chat_message", (data) => io.emit("phone_chat_message", data));
+
+});
 
 httpServer.listen(PORT, () => {
     console.log(`🚀 Server listening on port ${PORT}`);
