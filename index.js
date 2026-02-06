eb4a06c0f618776a99cfe0a4e1177726f461f19c
Serhii Frolov <larzofficialgames@gmail.com>
2026-02-06 13:49:51 +0200

    add pin

--- index.js
+++ index.js
@@ -25,11 +25,16 @@ io.on("connection", (socket) => {
     socket.on("request_sheet_data", (id) => io.emit("request_sheet_data", id));
     socket.on("receive_sheet_data", (data) => io.emit("receive_sheet_data", data));
 
-    // --- 🔥 НОВЕ: ВИБІР ПЕРСОНАЖА ---
-    // Телефон просить список доступних героїв
+    // --- ВИБІР ПЕРСОНАЖА ---
     socket.on("request_actor_list", () => io.emit("request_actor_list"));
-    // Foundry відправляє цей список
     socket.on("receive_actor_list", (list) => io.emit("receive_actor_list", list));
+
+    // --- 🔥 НОВЕ: ЛОГІКА БЕЗПЕКИ (PIN-КОД) ---
+    // Передаємо спробу входу від телефону до Foundry
+    socket.on("mobile_login_attempt", (data) => io.emit("mobile_login_attempt", data));
+    // Передаємо відповідь від Foundry до телефону
+    socket.on("login_success", () => io.emit("login_success"));
+    socket.on("login_failed", () => io.emit("login_failed"));
 
     // --- КИДКИ ---
     socket.on("mobile_roll_skill", (data) => io.emit("mobile_roll_skill", data));
